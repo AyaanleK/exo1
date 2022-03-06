03efe279a73c8b8bac8dd1788ff582928a03f6a6
Danish-Pirate <arja0015@stud.kea.dk>
2022-03-06 12:50:46 +0100

    changes

--- src/main/java/com/example/project/controller/ProjectController.java
+++ src/main/java/com/example/project/controller/ProjectController.java
@@ -6,18 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProjectController {
 
-  @GetMapping("/index")
-  public String index(){
-    return "page1.5";
-  }
-
-  @GetMapping("/page1")
-  public String page1(){
-    return "page1";
-  }
-
-  @GetMapping("/page2")
-  public String page2(){
+  @GetMapping("/")
+  public String index() {
     return "index";
+  }
+
+  @GetMapping("/hvad-kan-man-gøre")
+  public String page1() {
+    return "hvad-kan-man-gøre";
   }
 }
